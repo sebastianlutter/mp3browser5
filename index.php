94c2792cd982c51636fa31d9b7ca407a42efbcc7
Sebastian Lutter <sebastian.lutter22@gmail.com>
2020-01-29 14:27:14 +0100

    Put the right path for mediastation music

--- index.php
+++ index.php
@@ -3,20 +3,20 @@ require('mp3browser5.php');
 
 $config = array(
   # Where your MP3 music is available on the file system.
-  # Leave empty to use the current directory.  
+  # Leave empty to use the current directory.
   #	'path' => "/mnt/media/music", // Linux path sample
   #	'path' => "F:\MUSIC", // Windows path sample
-  'path' => "./music", // Current directory sample
-  
+  'path' => "/data/music", // Current directory sample
+
   # The template file
   'template' => "template.inc",
-  
+
   # Name of root entry in header
   'homeName' => "MP3 Web Browser with HTML5 Player",
 
   # Array of regular expression (regexp) matches for files/directories to hide
   'hideItems' => array("/^lost\+found/", "/^\./"),
-  
+
   # Filename character set.  Typical values are utf-8 (linux) or iso-8859-1 (windows).
   'charset' => "utf-8",
 
@@ -25,8 +25,8 @@ $config = array(
   'folderCovers' => true,
 
   # 'securePath' => false allows symlinks to folders outside the 'path' folder, but might be less secure.
-  'securePath' => true,    
-  
+  'securePath' => true,
+
   # Set to true to display PHP errors and notices.  Should be set to false.
   'debug' => false,
 );
